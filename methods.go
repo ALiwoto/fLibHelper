233e2e5440e8c5e638adac7aa3e494d54884aad3
Aliwoto <woto@kaizoku.cyou>
2023-05-19 03:58:07 +0330

    Add and implement invokeCommonPostRequest method.
    Signed-off-by: Aliwoto <woto@kaizoku.cyou>

--- methods.go
+++ methods.go
@@ -24,13 +24,36 @@ func (c *FLibClient) CollectGold(opt *ReqOptions) (string, error) {
 	if wholeData != "" {
 		data = strings.NewReader(wholeData)
 	}
-	req, err := http.NewRequest(http.MethodPost, c.BaseUrl+"/cards/collectgold", data)
+
+	return c.invokeCommonPostRequest("/cards/collectgold", data, len(wholeData))
+}
+
+func (c *FLibClient) LoadFP(opt *ReqOptions) (string, error) {
+	var data *strings.Reader
+	wholeData := ""
+	if opt != nil {
+		if opt.ReqData != "" {
+			wholeData = c.ReqDataName + "=" + opt.ReqData
+		} else if opt.ReqTotalData != "" {
+			wholeData = opt.ReqTotalData
+		}
+	}
+
+	if wholeData != "" {
+		data = strings.NewReader(wholeData)
+	}
+
+	return c.invokeCommonPostRequest("/player/load", data, len(wholeData))
+}
+
+func (c *FLibClient) invokeCommonPostRequest(path string, data io.Reader, contentLen int) (string, error) {
+	req, err := http.NewRequest(http.MethodPost, c.BaseUrl+"/player/load", data)
 	if err != nil {
 		return "", err
 	}
 
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
-	req.Header.Set("Content-Length", ssg.ToBase10(len(wholeData)))
+	req.Header.Set("Content-Length", ssg.ToBase10(contentLen))
 	req.Header.Set("User-Agent", c.UserAgent)
 	req.Header.Set("Host", c.HostString)
 	req.Header.Set("Connection", "close")
